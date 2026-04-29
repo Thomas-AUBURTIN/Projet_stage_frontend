2f077c893c21e2ef807ebf11f76afd33c35fa122
Thomas Auburtin <jeudego@hotmail.com>
2026-04-29 12:11:36 +0200

    proto_0.1.0

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ st.sidebar.title("ollama")
 st.sidebar.markdown("llm gratuit")
 
 pg = st.navigation([
-    st.Page("page_1.py", title="Accueil"),
+    st.Page("page_1.py", title="Accueill"),
     st.Page("page_2.py", title="historique"),
     
 ])
